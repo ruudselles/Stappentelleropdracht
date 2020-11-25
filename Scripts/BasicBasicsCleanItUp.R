ed27ba05487bb96be6bb5ba4bc2bfc74a2439051
NienjaLangerak <a.j.langerak.1@erasmusmc.nl>
2020-11-25 09:49:23 +0100

    update script new data

--- Scripts/BasicBasicsCleanItUp.R
+++ Scripts/BasicBasicsCleanItUp.R
@@ -21,12 +21,12 @@ rm(Data_Full_anonymous)
 #Opdracht variabele Namen Opschonen (CleanItUp Lesson 1, video 1)-----------
 library(janitor)
 
-CleanIntake <- clean_names(Data)
-view(CleanIntake)
+# Dit komt kunnen ze uitproberen, maar komt pas echt aanbod in Opdracht pipe je opschoonacties
+##CleanIntake <- clean_names(Data)  
 
 #Opdracht variabele herorganiseren (CleanItUp Lesson 1, video 2)-----------
-CleanIntake <- select(Data, geslacht, woon, everything()) #drie variabelen voorop zetten
-CleanIntake <- select(CleanIntake, -submitdate.x) #2 variabelen verwijderen
+DataSelect <- select(Data, geslacht, woon, everything()) #drie variabelen voorop zetten
+DataSelect <- select(DataSelect, -submitdate.x) # variabelen verwijderen
 
 
 #Opdracht Pipe je opschoonacties (CleanItUp Lesson 1, video 3)----
@@ -45,25 +45,25 @@ CleanData <- CleanData %>%
 
 #CLEAN IT UP 2----------
 #Opdracht sorteren van waarden (CleanItUp Lesson 1, video 1) ----
-  summary(data_combined)
+  summary(CleanData)
 
-data_omrom <- data_combined %>% 
+Data_Stappen <- CleanData %>% 
   arrange(desc(stap_om_1_aantal)) %>% 
   select(stap_om_1_aantal)
 
-data_omrom_vrouwen <- data_combined %>% 
+Data_Stappen_Vrouwen <- CleanData %>% 
   filter(geslacht == "Vrouw") %>% 
   arrange(desc(stap_om_1_aantal)) %>% 
   select(stap_om_1_aantal)
   
-data_omrom_mannen <- data_combined %>% 
+Data_Stappen_Mannen <- CleanData %>% 
   filter(geslacht == 'Man') %>% 
   arrange(desc(stap_om_1_aantal)) %>% 
   select(stap_om_1_aantal)
 
 
 #Opdracht Group_By CleanItUp 2, filmpje 2 ----
-data_combined_summarize <- data_combined %>% 
+ManVrouwUitwonendThuiswonend <- CleanData %>% 
   group_by(geslacht) %>% 
   summarize(maxsteps = max(stap_om_1_aantal, na.rm=TRUE), 
             minsteps = min(stap_om_1_aantal,na.rm=TRUE), 
@@ -73,7 +73,7 @@ data_combined_summarize <- data_combined %>%
 
 
 #Opdracht opslaan summary data frame (CleanItUp 2, filmpje 3) -----------
-ManVrouwUitThuis <- data_combined %>%   
+ManVrouwUitThuis <- CleanData %>%   
   group_by(geslacht, woon) %>% 
   summarize(maxsteps = max(stap_om_1_aantal, na.rm=TRUE), 
             minsteps = min(stap_om_1_aantal,na.rm=TRUE), 
@@ -83,16 +83,14 @@ ManVrouwUitThuis <- data_combined %>%
 
 
 #Opdracht Mutate (CleanItUp 3, filmpje 2 en 3) ------------
-data_combined <- data_combined %>% 
+CleanData <- CleanData %>% 
   mutate(stap_om_week_aantal = stap_om_1_aantal + stap_om_2_aantal+ stap_om_3_aantal+ stap_om_4_aantal+ 
                                 stap_om_5_aantal+ stap_om_6_aantal+ stap_om_7_aantal) 
 
-data_combined <- data_combined %>% 
+CleanData <- CleanData %>% 
   mutate(stap_om_week_aantal = stap_om_1_aantal + stap_om_2_aantal+ stap_om_3_aantal+ stap_om_4_aantal+ 
            stap_om_5_aantal+ stap_om_6_aantal+ stap_om_7_aantal) %>% 
   mutate(stap_om_1_10000 = stap_om_1_aantal > 10000)
 
-#opslaan van de data_combined file
-write.csv(data_combined, here("data", "data_combined.csv"))
-
-
+#opslaan van de CleanData file
+write.csv(CleanData, here("data", "CleanData.csv"))
